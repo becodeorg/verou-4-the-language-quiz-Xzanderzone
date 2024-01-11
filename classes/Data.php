1b3ad795cab7e5195cdb0b6eeb9238a95712a10d
PIeter <pieter_ophalvens@hotmail.com>
2024-01-11 10:12:02 +0100

    starter files --swedish

---
+++ classes/Data.php
@@ -0,0 +1,21 @@
+<?php
+
+class Data
+{
+  // This will provide the data you need
+  // No need to change any logic here - only data (to add or update words)
+  public static function words(): array
+  {
+    return [
+      'svenska' => 'swedish',
+      'bröd' => 'bread',
+      'trädgårdstomte' => 'garden gnome',
+      'ägg' => 'egg',
+      'buske' => 'bush',
+      'hatt' => 'hat',
+      'dörr' => 'door',
+      'musik' => 'music',
+      'chokladkaka' => 'chocolate cake',
+    ];
+  }
+}
